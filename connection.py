69a4a427302d502236b3850ee7d72487524e4826
Liam kelleher <91279431+Liamkelleher@users.noreply.github.com>
2024-04-10 09:43:44 -0400

    delete init funtion

--- connection.py
+++ connection.py
@@ -17,54 +17,6 @@ def connect():
 
     return connection, cursor
 
-#Add initial data if not included
-def initData():
-    query = """SELECT COUNT(*) FROM ClubMember"""
-    result = executeQuery(query)
-
-    if(result[0][0] == 0):
-        query = """INSERT INTO ClubMember (email, password, fname, lname) VALUES
-                ('bob@cm.com', 'bob', 'Bob', 'Z'),
-                ('bobby@cm.com', 'bobby', 'Bobby', 'Z')
-                """
-        executeQuery(query)
-
-    query = """SELECT COUNT(*) FROM trainer"""
-    result = executeQuery(query)
-
-    if(result[0][0] == 0):
-        query = """INSERT INTO trainer (email, password, fname, lname) VALUES
-                ('liamkelleher@trainer.com', 'liam', 'Liam', 'Kelleher'),
-                ('lilyczarnecki@trainer.com', 'lily', 'Lily', 'Czarnecki'),
-                ('hughhang@trainer.com', 'hugh', 'Hugh', 'Hang')
-                """
-        executeQuery(query)
-
-    query = """SELECT COUNT(*) FROM admin"""
-    result = executeQuery(query)
-
-    if(result[0][0] == 0):
-        query = """INSERT INTO admin (email, password, fname, lname) VALUES
-                ('admin1@admin.com', 'admin1', 'Admin', '1'),
-                ('admin2@admin.com', 'admin2', 'Admin', '2'),
-                ('admin3@admin.com', 'admin3', 'Admin', '3')
-                """
-        executeQuery(query)
-
-    #Training equipment check
-    #Check if data is in table
-    query = """SELECT COUNT(*) FROM trainingEquipment"""
-    result = executeQuery(query)
-
-    #Add data
-    if(result[0][0] == 0):
-        query = """INSERT INTO trainingEquipment (name, status) VALUES
-            ('Bench', 'Good'),
-            ('Squat rack', 'Decent'),
-            ('Treadmill', 'Not functional');
-            """
-        executeQuery(query)
-
 # helper for executing queries
 def executeQuery(query, parameters=()):
     connection, cursor = connect()
